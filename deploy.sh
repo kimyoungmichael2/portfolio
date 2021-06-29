596e38f6c25fea699f378f2ccd8a4c09344eb241
Michael Kim <kimyoungmichael2@gmail.com>
2021-06-29 12:17:43 -0400

    added public to files to be copied over to nginx

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #first, git pull in the server machine.
 #run "sudo bash deploy.sh" to copy the necessary files over to nginx directory and restart nginx
 
-cp -r ./{index.html,css} /var/www/michaelyoung.kim/
+cp -r ./{index.html,css,public} /var/www/michaelyoung.kim/
 echo copy complete!
 
 systemctl reload nginx
